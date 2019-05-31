dab27066bea5a92e89c6a9dfcbe41da1b8fee1c5
Khoirunnisa Afandi <nisaafandi@gmail.com>
2019-05-31 22:15:59 +0700

    Add files via upload
    Machine learning dengan metode linear regression dengan feature: Jumlah Penduduk, Kemiskinan, Pendapatan, Golput tahun 2014 dan tahun 2019, serta class yang terdiri dari 3 macam

---
+++ classification.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
+data = pd.read_csv('/Users/user/PycharmProjects/coba/Dataset.csv')
+data.dropna(inplace=True)
+data['JumlahPenduduk'] = data['JumlahPenduduk'].str.replace(',', '')
+data['JumlahPenduduk'] = data['JumlahPenduduk'].astype(float)
+data['Kemiskinan'] = data['Kemiskinan'].str.replace(',', '')
+data['Kemiskinan'] = data['Kemiskinan'].astype(float)
+data['Pendapatan'] = data['Pendapatan'].str.replace(',', '')
+data['Pendapatan'] = data['Pendapatan'].astype(float)
+data['Golput14'] = data['Golput14'].str.replace(',', '')
+data['Golput14'] = data['Golput14'].astype(float)
+data['Golput19'] = data['Golput19'].str.replace(',', '')
+data['Golput19'] = data['Golput19'].astype(float)
+# print(data.head())
+# print(data.dtypes)
+
+X = data[['JumlahPenduduk','Kemiskinan','Pendapatan','Golput14','Golput19']]
+y = data[['Class']]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+print(len(X_train))
+
+clf = LinearRegression()
+# clf.fit(X_train, y_train)
+clf.fit(X_train, y_train)
+LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
+print(clf.predict(X_test))
+print(y_test)
+print(clf.score(X_test, y_test))
+
+
+plt.scatter(data['Class'], data['JumlahPenduduk'], edgecolors='pink')
+plt.scatter(data['Class'], data['Kemiskinan'], edgecolors='brown')
+plt.scatter(data['Class'], data['Pendapatan'], edgecolors='grey')
+plt.scatter(data['Class'], data['Golput14'], edgecolors='yellow')
+plt.scatter(data['Class'], data['Golput19'], edgecolors='brown')
+plt.title('Grafik data golput, kemiskinan, pendapatan dan jumlah penduduk dibandingkan dengan Class')
+plt.show()
